535780de3b197669f7ef0f3fe9563aef73f23452
Fayndly <fayndly@gmail.com>
2023-10-23 19:44:57 +0400

    rewrote request validation

--- Validations/index.js
+++ Validations/index.js
@@ -18,17 +18,30 @@ export const loginValidation = [
 export const transactionCreateValidation = [
   body("type", "Неверный тип транзакции").isString(),
   body("shortDescription", "Введите краткое описание транзакции").isString(),
+  body(
+    "currency",
+    "Выберите валюту по которой была совершена транзакция"
+  ).isString(),
   body("amount", "Введите сумму транзакции").isFloat(),
   body("check", "Выберите счет").isString(),
-  body("date", "Выберите дату транзакции").isString(),
   body("category", "Выберите категорию транзакции").isString(),
+  body("time", "Выберите дату транзакции").isString(),
   body("fullDescription", "Не верный формат записи описания")
     .optional()
     .isString(),
 ];
 
 export const checkCreateValidation = [
+  body("isGeneral", "Выберите является ли  счет основным").isBoolean(),
   body("name", "Неверное имя счета").isString(),
   body("amount", "Неверное первоначальная сумма счета").isFloat(),
+  body("currency", "Выберите основную валюту счета").isFloat(),
   body("color", "Неправильный цвет").isHexColor(),
+];
+
+export const categoryCreateValidation = [
+  body("name", "Неверное имя категории").isString(),
+  body("type", "Неверный тип категории").isString(),
+  body("color", "Неправильный цвет").isHexColor(),
+  body("icon", "Неправильный иконка").optional().isString(),
 ];
