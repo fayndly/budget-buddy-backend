e234c52f6d42003fab4c8dd0bf8f2fb18269e51e
FAY <migoviprog@gmail.com>
2024-02-04 18:35:53 +0400

    corrected answers in currencies

--- Controllers/CurrencyController.js
+++ Controllers/CurrencyController.js
@@ -5,17 +5,23 @@ import serverErrorHandler from "../Utils/ServerErrorHandler.js";
 export const getAll = async (_, res) => {
   try {
     const currencies = await CurrencyModel.find();
+
     res.json(currencies);
   } catch (err) {
-    serverErrorHandler(res, err, "Не удалось найти валюты");
+    serverErrorHandler(res, err, "Не удалось получить валюты");
   }
 };
 
 export const getOneById = async (req, res) => {
   try {
     const currency = await CurrencyModel.findById(req.params.id);
+
+    if (!currency) {
+      return res.status(404).json({ message: "Не удалось найти валюту" });
+    }
+
     res.json(currency);
   } catch (err) {
-    serverErrorHandler(res, err, "Не удалось найти валюту");
+    serverErrorHandler(res, err, "Не удалось получить валюту");
   }
 };
